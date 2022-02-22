3cd4a2a5022029a532b10d1003639af0ef8828f8
dsokolo <31702434+dsokolo@users.noreply.github.com>
2022-02-21 21:57:17 -0500

    Add data download and correlation plotting functions.

---
+++ R/DEET_data_download.R
@@ -0,0 +1,113 @@
+#' @title DEET_data_download
+#'
+#' @description Function to automatically download the files within
+#' the DEET database that are required for the DEET_enrich
+#'  and DEET_feature_extractfunctions.
+#'
+#' @param x categorical variable containing options "ALL", "enrich",
+#' "metadata" or "feature_matrix".
+#'
+#'
+#' @return Named list with the neccesary data required to input into
+#' DEET_feature_extract or DEET_enrich. The metadata within DEET
+#' can also be downloaded.
+#'
+#' \itemize{
+#'   \item feature_matrix - A gene by comparison matrix populated
+#'   with the log2FC of gene expression for the genes that are
+#'   considered DE within the study (padj < 0.05).
+#'   \item metadata - a comparison - by - explanatory piece of data dataframe
+#'    providing important details to contextualize each study.
+#'    For every pairwise comparison, the study name,
+#'    source (SRA, TCGA, GTEx and SRA-manual),
+#'    description from the DRA compendium,
+#'    the number of samples (total, up-condition,
+#'    and down-condition), samples (total ,up-condition, down-condition),
+#'    tissue (including tumour from TCGA),
+#'    number of DEs (total, up-condition, down-condition),
+#'    age (mean +- sd), sex, top 15 DEGs - up, top 15 DEGs - down,
+#'    top 5 enriched pathways, and top 5 enriched TFs.
+#'    PMID are also available for studies selected from SRA.
+#'    Lastly, each pairwise comparison was given an overall
+#'    category based on those decided in Crow et al., 2019.
+#'   \item DEET_enrich - A named list of seven objects containing
+#'    the data frames summarizing the DEGs from comparisons within
+#'    DEET, GMT objects of comparisons within DEET for enrichment
+#'    through ActivePathways, GMT objects for basic pathway and TF
+#'    enrichment, and a dataframe for the metadata of each study.
+#'    For more detail on each element of the list, please consult
+#'    the vignette or "?DEET_example_data", as it is a subset
+#'    of this object
+#' }
+#'
+#' @author Dustin Sokolowski, Jedid Ahn
+#'
+#' @examples
+#' \donttest{
+#'
+#' # Download the metadata. Downloading other
+#' # files within DEET are larger and take
+#' # a bit more time.
+#' downloaded <- DEET_data_download(x = "metadata")
+#'
+#' # extract metadata from the list
+#' metadata <- downloaded[["metadata"]]
+#'
+#'}
+#'
+#' @references
+#' Engebretsen, S., & Bohlin, J. (2019). Statistical predictions with glmnet.
+#' Clinical epigenetics, 11(1), 1-3.
+#'
+#' @export
+#' @importFrom downloader download
+#'
+DEET_data_download <- function(x = "ALL") {
+  inputCheck <- c(x %in% c("ALL", "enrich", "metadata", "feature_matrix"))
+  if(!inputCheck) {
+  stop("Inputted variable was not in options: ALL, enrich, metadata, feature_matrix.
+  Case sensitive. Please input one of these options or download manually from
+  http://wilsonlab.org/public/DEET_data/.")
+  }
+
+  l <- list()
+
+  if(x == "ALL") {
+    x <- c("enrich", "metadata", "feature_matrix")
+  }
+
+  for(i in x) {
+    if(i %in% "metadata") {
+      datafile <- "DEET_metadata.rda"
+      metafile <- paste0(datafile)
+      url <- paste0("http://wilsonlab.org/public/DEET_data/",
+                    metafile, "?raw=true")
+      destfile <- file.path(tempdir(), metafile)
+      downloader::download(url, destfile = destfile, mode = "wb")
+      load(destfile)
+      l[["metadata"]] <- DEET_metadata
+    }
+    if(i %in% "enrich") {
+      datafile <- "DEET_combined.rda"
+      metafile <- paste0(datafile)
+      url <- paste0("http://wilsonlab.org/public/DEET_data/",
+                    metafile, "?raw=true")
+      destfile <- file.path(tempdir(), metafile)
+      downloader::download(url, destfile = destfile, mode = "wb")
+      load(destfile)
+      l[["DEET_enrich"]] <- DEET_combined
+    }
+    if(i %in% "feature_matrix") {
+      datafile <- "DEET_log2FC_matrix.rda"
+      metafile <- paste0(datafile)
+      url <- paste0("http://wilsonlab.org/public/DEET_data/",
+                    metafile, "?raw=true")
+      destfile <- file.path(tempdir(), metafile)
+      downloader::download(url, destfile = destfile, mode = "wb")
+      load(destfile)
+      l[["DEET_feature_extract"]] <- DEET_log2FC_matrix
+    }
+  }
+  return(l)
+}
+
---
+++ R/DEET_plot_correlation.R
@@ -0,0 +1,75 @@
+#' @title DEET_plot_correlation
+#'
+#' @description Take significant correlation outputs and generate
+#' scatterplots of the genes DE in one or the other.
+#'
+#' @param correlation_input The "DE_correlations" element of the
+#' output of the DEET_enrich function. This function only works if
+#' there is at least one significantly correlated study.
+#'
+#' @return Named list of ggplot objects with the correlation between
+#' the input study and the study within DEET
+#'
+#' @author Dustin Sokolowski, Jedid Ahn
+#'
+#' @examples
+#' \donttest{
+#'
+#' data("example_DEET_enrich_input")
+#' data("DEET_example_data")
+#' DEET_out <- DEET_enrich(example_DEET_enrich_input, DEET_dataset = DEET_example_data)
+#' correlation_input <- DEET_out$DE_correlations
+#' correlation_plots <- DEET_plot_correlation(correlation_input)
+#'
+#'}
+#'
+#' @export
+#' @importFrom ggplot2 ggplot aes geom_point scale_color_manual geom_text_repel theme_bw xlab ylab theme element_text
+#' @importFrom ggrepel geom_text_repel
+#'
+
+
+
+DEET_plot_correlation <- function(correlation_input ) {
+
+if(!is.list(correlation_input)) {
+  stop("correlation_input is not of class list.")
+}
+if(nrow(correlation_input$results) < 1) {
+  stop("There were no significant correlations, ")
+}
+
+  # get DEET IDs
+  distNames <- names(correlation_input$distributions)
+  message(paste0("There are ", length(distNames), " comparisons with a significant correlation."))
+
+  rownames(correlation_input$metadata) <- correlation_input$metadata$DEET.ID
+
+  output_plots <- list()
+
+  for(i in distNames) {
+    metaColumn <- correlation_input$metadata[i,]
+
+    plts <- distribution <- correlation_input$distributions[[i]]
+
+    name <- rep("", nrow(plts))
+    name[plts$color != "grey"] <- rownames(plts)[plts$color != "grey"]
+    plts$label <- name
+    plts$color <- as.factor(plts$color)
+    ttl <- metaColumn[i,"DEET.Name"][[1]]
+    cor_out <- correlation_input$results[i,]
+    cor_out1 <- cor_out[i,"Spear"]
+    corLab <- round(cor_out1,3)
+
+    p <- ggplot2::ggplot(plts, ggplot2::aes(x=input, y=DEET, color=color, label=label)) + ggplot2::geom_point() +
+      ggplot2::scale_color_manual(values=c("grey", "orange", "purple")) +
+      ggrepel::geom_text_repel(size=2,max.overlaps = 30) + ggplot2::theme_bw() +
+      ggplot2::xlab(paste0("Input coefficient Rho: ", corLab)) + ggplot2::ylab(paste0("DEET-",ttl)) + ggplot2::theme(axis.title.y = ggplot2::element_text(size = 8))
+
+    output_plots[[i]] <- p
+
+  }
+
+  return(output_plots)
+
+}
