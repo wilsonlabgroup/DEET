be5122af83f4d2df82144b81b23c69230a476665
dsokolo <31702434+dsokolo@users.noreply.github.com>
2022-11-24 13:05:20 -0500

    For the correlation analysis, return comparisons with an FDR of 0.1 instead of 0.05. Users can still adjust threshold later but testing has suggested that these enriched comparisons with a slightly less significantly correlated set of intersecting DEGs can still be interesting.

--- R/DEET_enrich.R
+++ R/DEET_enrich.R
@@ -426,11 +426,11 @@ DEET_enrich <- function(DEG_list, DEET_dataset, ordered = FALSE, background = NU
       cor_mats <- lapply(tst, function(x) return (x$matrix))
 
       # Make sure that correlations are significant with Pearson or Spearman.
-      cor_results_sig <- cor_results[cor_results$FDR_Pear < 0.05 |
-                                       cor_results$FDR_Spear < 0.05, ]
+      cor_results_sig <- cor_results[cor_results$FDR_Pear < 0.1 |
+                                       cor_results$FDR_Spear < 0.1, ]
 
-      cor_mats_sig <- cor_mats[cor_results$FDR_Pear < 0.05 |
-                                 cor_results$FDR_Spear < 0.05]
+      cor_mats_sig <- cor_mats[cor_results$FDR_Pear < 0.1 |
+                                 cor_results$FDR_Spear < 0.1]
 
       rownames(cor_results_sig) <- cor_results_sig$DEET.ID
       names(cor_mats_sig) <- cor_results_sig$DEET.ID
